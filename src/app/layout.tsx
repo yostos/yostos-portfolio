c08d21b2fb69981a10606bdafe1dd4f04d1d9a89
Toshiyuki Yoshida <yostos@yostos.org>
2025-03-05 14:18:16 +0900

    added Tinylitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const tinylyticsId = process.env.NEXT_PUBLIC_TINYLYTICS_ID;
   return (
     <html lang="en" data-theme="portfolioTheme">
       <body
@@ -32,6 +34,12 @@ export default function RootLayout({
         <Header />
         <div className="flex-grow">{children}</div>
         <Footer />
+        {tinylyticsId && (
+          <Script
+            src={`https://tinylytics.app/embed/${tinylyticsId}.js`}
+            strategy="afterInteractive"
+          />
+        )}
       </body>
     </html>
   );
